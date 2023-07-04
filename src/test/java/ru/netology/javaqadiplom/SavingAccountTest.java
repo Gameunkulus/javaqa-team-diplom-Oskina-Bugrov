14198e84947f47425a954e93fadfb907b73ccee0
Oskina Margarita <rita_2113@mail.ru>
2023-07-04 18:16:26 +0300

    Added a few more tests for SavingAccount

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,6 +18,65 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+    @Test
+    public void shouldDeductLessThanInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(-1_000);
+
+        Assertions.assertEquals(2_000 - 1_000, account.getBalance());
+    }
+    @Test
+    public void shouldDeductInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                0,
+                10_000,
+                5
+        );
+
+        account.add(-2_000);
+
+        Assertions.assertEquals(2_000 - 2_000, account.getBalance());
+    }
+
+        SavingAccount account = new SavingAccount(
+            2_000,
+            1_000,
+            50_000,
+            3);
+
+    // Выводим начальный баланс.
+    @Test
+    public void shouldGetInitialBalance() {
+        int expected = 2_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Выводим минимальный баланс.
+    @Test
+    public void shouldGetMinBalance() {
+        int expected = 1_000;
+        int actual = account.getMinBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Выводим максимальный баланс.
+    @Test
+    public void shouldGetMaxBalance() {
+        int expected = 50_000;
+        int actual = account.getMaxBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     public void shouldAnExceptionMustBeThrownWhenMinGreaterMax() {
@@ -29,7 +88,6 @@ public class SavingAccountTest {
                     3);
         });
     }
-
     @Test
     public void shouldAnExceptionMustBeThrownWhenInitialLessMin() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -51,6 +109,5 @@ public class SavingAccountTest {
                     3);
         });
     }
-
 }
 
