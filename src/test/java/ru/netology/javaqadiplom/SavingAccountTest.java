f42bf21efb4826b49b0fbdc57ebe8a8cd3b0c2f2
Oskina Margarita <rita_2113@mail.ru>
2023-07-05 11:12:09 +0300

    Added a few tests and reformat code

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
+    //Операции по счету:
+
+    //Пополнение счета
     @Test
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
@@ -18,8 +21,28 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    //Пополнение на 0 руб, сумма баланса остается не измененной:
     @Test
-    public void shouldDeductLessThanInitialBalance() {
+    public void shouldReturnNewBalanceForNullAmountAdd() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(0);
+
+        int expected = 2_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Попытка пополнить баланс на отрицательное число:
+    @Test
+    public void shouldTryTopUpNegativeAmountAdd() {
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -29,23 +52,100 @@ public class SavingAccountTest {
 
         account.add(-1_000);
 
-        Assertions.assertEquals(2_000 - 1_000, account.getBalance());
+        int expected = 2_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
     }
+
+    //Пополнение на сумму больше максимального баланса
     @Test
-    public void shouldDeductInitialBalance() {
+    public void shouldTopUpAmountWhenNewBalanceGreaterMax() {
         SavingAccount account = new SavingAccount(
                 2_000,
-                0,
+                1_000,
                 10_000,
                 5
         );
 
-        account.add(-2_000);
+        account.add(55_000);
 
-        Assertions.assertEquals(2_000 - 2_000, account.getBalance());
+        int expected = 2_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
     }
 
+
+    //Покупка на сумму в пределах текущего баланса на положительную сумму покупки
+    @Test
+    public void shouldDeductLessThanInitialBalance() {
         SavingAccount account = new SavingAccount(
+                5_000,
+                0,
+                100_000,
+                5);
+
+        account.pay(1_000);
+
+        int expected = 4_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //При покупке на положительное число равному текущему балансу
+    @Test
+    public void shouldDeductInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                5_000,
+                0,
+                100_000,
+                5);
+
+        account.pay(5_000);
+
+        int expected = 0;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //При покупке на 0 руб, баланс не должен изменится
+    @Test
+    public void shouldReturnNewBalanceForNullAmountPay() {
+        SavingAccount account = new SavingAccount(
+                5_000,
+                0,
+                100_000,
+                5);
+
+        account.pay(0);
+
+        int expected = 5_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // При покупке на отрицательное число рублей операция не пройдет и баланс не изменится:
+    @Test
+    public void shouldReturnNewBalanceForNegativeAmountPay() {
+        SavingAccount account = new SavingAccount(
+                5_000,
+                0,
+                100_000,
+                5);
+
+        account.pay(-5_000);
+
+        int expected = 5_000;
+        int actual = account.getBalance();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    SavingAccount account = new SavingAccount(
             2_000,
             1_000,
             50_000,
@@ -77,6 +177,7 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Должно выкидываться исключение в случае, если минимальный баланс больше максимального
 
     @Test
     public void shouldAnExceptionMustBeThrownWhenMinGreaterMax() {
@@ -85,9 +186,11 @@ public class SavingAccountTest {
                     1_000,
                     55_000,
                     20_000,
-                    3);
+                    5);
         });
     }
+
+    //Должно выкидываться исключение в случае, если начальный баланс меньше минимального
     @Test
     public void shouldAnExceptionMustBeThrownWhenInitialLessMin() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -99,15 +202,44 @@ public class SavingAccountTest {
         });
     }
 
+    //Должно выкидываться исключение в случае, если начальный баланс больше максимального
     @Test
     public void shouldAnExceptionMustBeThrownWhenInitialGreaterMax() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             SavingAccount account = new SavingAccount(
-                    50_000,
+                    25_000,
+                    5_000,
                     15_000,
-                    45_000,
                     3);
         });
     }
+
+    //Должно выкидываться исключение в случае, если указан отрицательный процент
+    @Test
+    public void shouldAnExceptionMustBeThrownWhenNegativePercent() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    25_000,
+                    1_000,
+                    50_000,
+                    -3);
+        });
+    }
+
+    //Расчет % на остаток
+    @Test
+    public void shouldCalculateYearChange() {
+        SavingAccount account = new SavingAccount(
+                25_000,
+                1_000,
+                50_000,
+                5);
+
+        int expected = 1_250;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
 
