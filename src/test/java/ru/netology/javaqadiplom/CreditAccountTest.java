ee0383bed49749c38b23bb30e97a49d6394e0664
Gameunkulus <bugr24-8@mail.ru>
2023-07-05 14:23:26 +0300

    Update CreditAccoutntTest. Bugrov

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,89 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void IllegalArgumentExceptionTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    -2_000,
+                    -5_000,
+                    -15
+            );
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    -2_000,
+                    5_000,
+                    15
+            );
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    2_000,
+                    -5_000,
+                    15
+            );
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    3_000,
+                    5_000,
+                    -15
+            );
+        });
+
+    }
+
+    @Test
+    void payTest() {
+        CreditAccount account = new CreditAccount(
+                3_000,
+                5_000,
+                15
+        );
+
+        account.pay(50);
+        int expected = 2950;
+
+        Assertions.assertEquals(expected, account.getBalance());
+        Assertions.assertFalse(account.pay(-50));
+        Assertions.assertFalse(account.pay(3000));
+    }
+
+
+    @Test
+    void add() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertTrue(account.add(50));
+        Assertions.assertFalse(account.add(-50));
+        account.add(50);
+        int expexted = 100;
+
+        Assertions.assertEquals(expexted, account.getBalance());
+    }
+
+    @Test
+    void yearChange() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+        int expected = 0;
+
+        Assertions.assertEquals(expected, account.yearChange());
+        account.pay(200);
+        expected = -30;
+        Assertions.assertEquals(expected, account.yearChange());
+    }
+
 }
